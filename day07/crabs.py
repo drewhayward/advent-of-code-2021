041c4c44d96809513b3f9410acbd1c546456406e
Drew Hayward <d.andrew.hayward@gmail.com>
2021-12-07 13:42:55 -0500

    day7 linear part 2

--- day07/crabs.py
+++ day07/crabs.py
@@ -1,8 +1,10 @@
+import math
 test = """16,1,2,0,4,2,7,1,2,14"""
 
 def parse_input(contents):
     return list(map(int, contents.split(',')))
 
+# O(C * R)
 def min_pos(crabs, cost_fn= lambda x,y: abs(x - y)):
     num_spots = max(crabs) - min(crabs)
 
@@ -15,6 +17,28 @@ def min_pos(crabs, cost_fn= lambda x,y: abs(x - y)):
         best = min(best, cost)
 
     return best
+        
+# O (C)
+def min_pos2(crabs):
+    def cost_fn(a, b):
+        dist = abs(a - b)
+        return (dist * (dist + 1)) / 2
+
+    def total_cost(pos):
+        cost = 0
+        for crab in crabs:
+            cost += cost_fn(crab, pos)
+
+        return cost
+
+    pos = sum(crabs) / len(crabs)
+    return int( # answer is on of the ints beside the mean
+                min(
+                    total_cost(math.floor(pos)),
+                    total_cost(math.ceil(pos))
+                )
+            )
+
         
 
 if __name__ == "__main__":
@@ -29,9 +53,5 @@ if __name__ == "__main__":
     print(min_pos(crabs))    
 
     print('--- Part 2 ---')
-    
-    def cost2(a, b):
-        dist = abs(a - b)
-        return (dist * (dist + 1)) / 2
 
-    print(min_pos(crabs, cost2))
+    print(min_pos2(crabs))
